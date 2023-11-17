b6242b49cb690d17c649163b46c3dfc2cdcc8ee0
vamshikyatham <125437866+vamshikyatham@users.noreply.github.com>
2023-11-17 10:26:21 +0530

    Create minimum.py
    minimum of two numbers

---
+++ minimum.py
@@ -0,0 +1,10 @@
+#minimum of two numbers
+def min(a,b):
+    if a<=b:
+        print("a is minimum")
+    else:
+        print("b is minimum")
+#main code
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+min(a,b)
