96e2d597e5c63b1a7ba96c4b334ef61103114da4
vamshikyatham <125437866+vamshikyatham@users.noreply.github.com>
2023-11-17 10:24:46 +0530

    Create maximum.py
    maximum of two numbers

---
+++ maximum.py
@@ -0,0 +1,10 @@
+#maximum of two numbers
+def max(a,b):
+    if a>=b:
+        print("a is maximum")
+    else:
+        print("b is maximum")
+#main code
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+max(a,b)
