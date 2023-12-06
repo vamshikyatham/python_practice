7aa9349ee6a874cc2f906ef62a69530e533cb2a0
vamshikyatham <125437866+vamshikyatham@users.noreply.github.com>
2023-12-06 15:15:07 +0530

    Create patten_python.py

---
+++ patten_python.py
@@ -0,0 +1,28 @@
+"""
+p
+py
+pyt
+pyth
+pytho
+python
+pytho
+pyth
+pyt
+py
+p
+"""
+def python(n):
+    x=0;
+    for i in n:
+        x=x+1
+        print(n[0:x])
+    for j in n:
+        x=x-1
+        print(n[0:x])
+python("python")
+pyth
+pyt
+py
+p
+
+> 
