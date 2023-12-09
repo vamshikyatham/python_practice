173ce92c1d55996697369598876a83738386abdf
vamshikyatham <125437866+vamshikyatham@users.noreply.github.com>
2023-12-09 13:01:29 +0530

    Create string_change_the_char.py

---
+++ string_change_the_char.py
@@ -0,0 +1,16 @@
+def change(s,p,ch):
+    l=list(s)
+    l[p]=ch
+    print("".join(l))
+s=input("s=")
+p=int(input("p="))
+ch=input("ch=")
+change(s,p,ch)
+"""
+input=
+s=vamshi
+p=5
+ch=k
+
+output=vamshk
+"""
