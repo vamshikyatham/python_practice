b12b908c5fe339276b3f5528b4e2c5ff0c0cc878
vamshikyatham <125437866+vamshikyatham@users.noreply.github.com>
2023-12-12 15:11:10 +0530

    Create guess_the_number.py

---
+++ guess_the_number.py
@@ -0,0 +1,18 @@
+import random
+
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while True:
+        guess = int(input(f"Guess a number between 1 and {x}:"))
+        if guess < random_number:
+            print("sorry, guess again. Too low")
+        elif guess > random_number:
+            print("sorry, guess again. Too high")
+        else:
+            print(f"Yay, congrats. You have guessed the number {random_number}")
+            break
+
+
+guess(10)
