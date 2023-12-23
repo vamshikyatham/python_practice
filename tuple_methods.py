e09122f41a4ae4dd6cc4f691f552649615cb4bc6
vamshikyatham <125437866+vamshikyatham@users.noreply.github.com>
2023-12-23 14:31:21 +0530

    Create tuple_methods.py

---
+++ tuple_methods.py
@@ -0,0 +1,11 @@
+# tuple --> immutable ,unchangeable, allow duplicates, order
+
+# count
+tuple1 = (1, 2, 3, 1, 2, 3, 1, 4, 6, 1, 5)
+x = tuple1.count(1)
+print("Return the number of times it was occur:", x)
+
+# index
+x = tuple1.index(3)
+print("Return the index of the element of the first :", x)
+
