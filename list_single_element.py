6e2b807098291922a5c24f2a58603b09d85021f7
vamshikyatham <125437866+vamshikyatham@users.noreply.github.com>
2024-01-12 19:53:35 +0530

    Update list_single_element.py

--- list_single_element.py
+++ list_single_element.py
@@ -13,3 +13,24 @@ def single(list2):
 list1 = [1, 2, 3, 3, 2, 1, 3, 4]
 single(list1)
 
+"""
+class solution:
+    def single(self, list2):
+        self.list2 = list2
+        print(self.list2)
+        length = len(self.list2)
+        for i in range(length):
+            count = 0
+            for j in range(length):
+                if self.list2[i] == self.list2[j]:
+                    count += 1
+            if count == 1:
+                print("Single element in the list is :", self.list2[i])
+
+
+ob = solution()
+list1 = [1, 2, 3, 3, 2, 1, 3, 4]
+ob.single(list1)
+
+"""
+
